a56e2f2fcacd7144c3f370fd213fde20f1ca5749
Laura R <102334514+laurar7@users.noreply.github.com>
2022-08-09 20:24:12 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import NameTag from "./NameTag";
+
+import "./styles.css";
+
+const App = () => (
+  <div className="App">
+    <h1>Name Tag Generator</h1>
+    <NameTag />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
